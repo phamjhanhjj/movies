ee5f0a2d59ba68fb11495ce4589b7dd28cabe3ce
phamjhanhj <22010503@st.phenikaa-uni.edu.vn>
2024-09-23 13:05:16 +0700

    Initial commit

---
+++ movies/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+import 'package:movies/app.dart';
+import 'package:movies/firebase_options.dart';
+
+void main() async {
+ WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp(
+   options: DefaultFirebaseOptions.currentPlatform,
+ );
+
+
+ runApp(const MyApp());
+}
